3c38547cbabccecaedb017f0ef34de4399f50723
martinschobben <m.a.n.schobben@uu.nl>
2021-05-25 14:33:21 +0200

    export datasaurus_dozen

--- R/PAGES.R
+++ R/PAGES.R
@@ -5,13 +5,12 @@
 #'
 #' @import dplyr
 #' @import tidyr
-#' @import datasauRus
+#'
 #' @importFrom readr read_csv
 #' @importFrom modelr add_residuals
 #' @importFrom thematic thematic_on
 #' @importFrom ggplot2 ggplot aes facet_wrap facet_grid geom_point geom_area geom_boxplot geom_smooth vars theme_set theme_classic
 #'
-#' @export ggplot aes facet_wrap facet_grid geom_point geom_area geom_boxplot geom_smooth vars
 #' @docType package
 #' @name point
 NULL
---
+++ R/datasaurus_dozen.R
@@ -0,0 +1,15 @@
+#' datasauRus dozen
+#'
+#' See \code{datasauRus::\link[datasauRus:datasaurus_dozen]} for details.
+#'
+#' @format A data frame with 1846 rows and 3 variables:
+#' \describe{
+#'   \item{dataset}{indicates which dataset the data are from}
+#'   \item{x}{x-values}
+#'   \item{y}{y-values}
+#' }
+#'
+#' @export
+datasaurus_dozen <- datasauRus::datasaurus_dozen
+
+
